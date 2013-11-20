2e0fd40b3b40b3b3000e61a13de6edd70af0a030
allblackt <allblack007@gmail.com>
2013-11-20 20:37:39 +0200

    New error messages for Sports Pass.

--- src/com/tudor/sdm/Constants.java
+++ src/com/tudor/sdm/Constants.java
@@ -34,7 +34,13 @@ public class Constants {
 	public enum StringNames {
 		TITLE("title"),
 		WINDOW_MANAGE_CLIENTS_TITLE("window_manage_clients_title"),
-		BTN_MANAGE_CLIENTS_LABEL("btn_manage_clients_label");
+		BTN_MANAGE_CLIENTS_LABEL("btn_manage_clients_label"),
+		
+		//Errors
+		ERR_NOT_ENOUGH_SESSIONS_LEFT_ON_PASS("err_not_enough_sessions_left_on_pass"),
+		ERR_MAXSESSIONS_MUST_BE_GREATER_THAN_REMAININGSESSIONS("err_maxsessions_must_be_greater_than_remainingsessions"),
+		ERR_MAXSESSIONS_MUST_BE_GREATER_THAN_ZERO("err_maxsessions_must_be_greater_than_zero"),
+		ERR_REMAININGSESSIONS_MUST_BE_A_POSITIVE_NUMBER("err_remainingsessions_must_be_a_positive_number");
 		String stringName;
 		
 		private StringNames(String stringName) {
