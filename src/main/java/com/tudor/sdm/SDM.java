76ad7585b278dbecd4b4d56f19d83720aab42005
All Black <allblack007@gmail.com>
2014-03-31 21:36:28 +0200

    Changed to use the newly implemented locking mechanism in order to limit its instances to 1.

--- src/main/java/com/tudor/sdm/SDM.java
+++ src/main/java/com/tudor/sdm/SDM.java
@@ -1,23 +1,24 @@
 package com.tudor.sdm;
 
-import javax.swing.SwingUtilities;
-
+import com.tudor.sdm.ui.WelcomeWindow;
 import org.apache.log4j.Logger;
 
-import com.tudor.sdm.ui.WelcomeWindow;
+import javax.swing.*;
 
 public class SDM {
 
-	private static final Logger log = Logger.getLogger(SDM.class.getName()); 
+	private static final Logger log = Logger.getLogger(SDM.class.getName());
 	public static void main(String[] args) {
-		
-		log.info("Started app from " + System.getProperty("user.dir"));
-			SwingUtilities.invokeLater(new Runnable() {
-				@Override
-				public void run() {
-					new WelcomeWindow();
-				}
-			});
+        if(!(new SingleInstanceChecker().isAppActive())) {
+            log.info("Started app from " + System.getProperty("user.dir"));
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    new WelcomeWindow();
+                }
+            });
+        } else {
+            log.info("Application is already running...");
+        }
 	}
-
 }
