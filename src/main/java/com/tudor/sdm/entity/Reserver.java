66f10d9038d7087569bfc358424082ef6bbaf4f7
Tudor Tabacel <tudor.tabacel@avira.com>
2014-05-01 21:50:08 +0200

    Reserver interface for all the ones to be able to reserve a field.

--- src/main/java/com/tudor/sdm/entity/Reserver.java
+++ src/main/java/com/tudor/sdm/entity/Reserver.java
@@ -2,6 +2,7 @@ package com.tudor.sdm.entity;
 
 /**
  * Created by tudor on 01/05/14.
+ * Interface to be implemented by all the ones which will be able to reserve a field.
  */
 public interface Reserver {
     public String getName();
