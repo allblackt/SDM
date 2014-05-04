a474092870312d775dcae022421e218ff43d9ffd
Tudor Tabacel <tudor.tabacel@avira.com>
2014-05-04 22:12:29 +0200

    Added builder pattern and fine tune to Trainer class.

--- src/main/java/com/tudor/sdm/entity/Trainer.java
+++ src/main/java/com/tudor/sdm/entity/Trainer.java
@@ -2,6 +2,7 @@ package com.tudor.sdm.entity;
 
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -13,6 +14,7 @@ import javax.persistence.*;
 @Entity
 @Getter(AccessLevel.PUBLIC)
 @Setter(AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class Trainer implements Reserver {
 
     @Id
@@ -24,4 +26,31 @@ public class Trainer implements Reserver {
 
     @Column(length = 100)
     private String phoneNumber;
+
+    private Trainer(Builder builder) {
+        setName(builder.name);
+        setPhoneNumber(builder.phoneNumber);
+    }
+
+    public static final class Builder {
+        private String name;
+        private String phoneNumber;
+
+        public Builder() {
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder phoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Trainer build() {
+            return new Trainer(this);
+        }
+    }
 }
